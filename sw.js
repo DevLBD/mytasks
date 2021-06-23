bfa1a8591db2df3548e96bcf589a4748ed1b042a
Lorenzo Barretta <43916566+devlbd@users.noreply.github.com>
2021-06-23 22:18:28 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -35,7 +35,7 @@ self.addEventListener("fetch", function(event){
 });
 
 self.addEventListener("activate", function(event){
-    var cacheWhiteList = ["mytasks-v8-0-2", "gumroad-calculator-v2-2"];
+    var cacheWhiteList = ["mytasks-v8-0-2", "gumroad-calculator-v2-3"];
     
     event.waitUntil(
         caches.keys().then(function(cacheNames){
